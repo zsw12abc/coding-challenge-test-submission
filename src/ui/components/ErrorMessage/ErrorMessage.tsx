415c993004b2354159ca16f17668cd7f52dfe7f3
SW <11704698+zsw12abc@users.noreply.github.com>
2026-01-16 08:43:35 +1100

    Refactor forms to use `useForm` hook, replace repetitive logic with reusable components, and enhance error/validation handling

---
+++ src/ui/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+﻿import React, { FunctionComponent } from "react";
+import $ from "./ErrorMessage.module.css";
+
+interface ErrorMessageProps {
+  message?: string;
+}
+
+const ErrorMessage: FunctionComponent<ErrorMessageProps> = ({ message }) => {
+  if (!message) return null;
+  return <div className={$.error}>{message}</div>;
+};
+
+export default ErrorMessage;
